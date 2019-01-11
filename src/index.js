4a5dbf2693243ce48d3bfaa61f80c57657358fd9
Grasseh <steve@grasseh.com>
2019-01-11 15:20:02 -0500

    Revert "Add uncovered line"
    This reverts commit d643de04854649e10afe9e017f772d957c4be564.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ class EntryController{
     init(){
         console.info('Hello World');
     }
-
-    notCalledFn(){
-        console.log('HAHAHA');
-    }
 }
 
 let entry = new EntryController();
