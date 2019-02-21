c135e3720f536930d4bc767143801058b51b2e99
Louis-Philippe Bonenfant <louis-philippe@louismbp.local>
2019-02-21 15:33:02 -0500

    seperated md files for end to end and interpreter tests

--- test/integration/MarkdownParser.js
+++ test/integration/MarkdownParser.js
@@ -7,7 +7,7 @@ describe('MarkdownParser', function() {
     describe('parseFile', function() {
         it('returns testsuitedescriptor with correct invoker and # of tests', function() {
             let markdownParser = new MarkdownParser();
-            let descriptor = markdownParser.parseFile(`${__dirname}/artifacts/markdown/test1.md`);
+            let descriptor = markdownParser.parseFile(`${__dirname}/artifacts/markdown-interpreter/test2.md`);
             assert.strictEqual(descriptor.getInvoker(), 'PhpInvoker');
             assert.strictEqual(descriptor.getTests().length, 2);
         });
