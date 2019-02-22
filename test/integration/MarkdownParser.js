4d294ecba40a34e8b6950287ed397564ee7980c1
Louis-Philippe Bonenfant <louis-philippe@louismbp.local>
2019-02-21 20:04:27 -0500

    updated test to reflect support for params

--- test/integration/MarkdownParser.js
+++ test/integration/MarkdownParser.js
@@ -5,11 +5,14 @@ const MarkdownParser = require('../../src/interpreter/MarkdownParser');
 
 describe('MarkdownParser', function() {
     describe('parseFile', function() {
-        it('returns testsuitedescriptor with correct invoker and # of tests', function() {
+        it('returns testsuitedescriptor with correct invoker, # of tests and amount/value of parameters', function() {
             let markdownParser = new MarkdownParser();
             let descriptor = markdownParser.parseFile(`${__dirname}/artifacts/markdown-interpreter/test2.md`);
             assert.strictEqual(descriptor.getInvoker(), 'PhpInvoker');
             assert.strictEqual(descriptor.getTests().length, 2);
+            assert.strictEqual(descriptor.getTests()[1].getParameters().length, 2);
+            assert.strictEqual(descriptor.getTests()[1].getParameters()[0], '1');
+            assert.strictEqual(descriptor.getTests()[1].getParameters()[1], '1');
         });
     });
 });
