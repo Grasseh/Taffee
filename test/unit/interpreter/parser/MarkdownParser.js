41818c63641343c6941efd56620c76d25b8972de
Louis-Philippe Bonenfant <louis-philippe@louismbp.local>
2019-03-10 18:46:40 -0400

    Added test to cover no elements case

--- test/unit/interpreter/parser/MarkdownParser.js
+++ test/unit/interpreter/parser/MarkdownParser.js
@@ -25,4 +25,12 @@ describe('MarkdownParser', function() {
             assert.strictEqual(descriptor.getTests()[3].getParameters()[1], 'C');
         });
     });
+
+    describe('empty markdown', function() {
+        it('returns testsuitedescriptor with no tests', function() {
+            let markdownParser = new MarkdownParser();
+            let descriptor = markdownParser.parseFile(`${__dirname}/artifacts/noElements.md`);
+            assert.strictEqual(descriptor.getTests().length, 0);
+        });
+    });
 });
