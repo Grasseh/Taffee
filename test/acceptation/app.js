076478ceb1d3edc7ed1421a2cce005ba9e7277e0
Grasseh <steve@grasseh.com>
2019-03-11 21:44:13 -0400

    Move acceptation test

--- test/acceptation/app.js
+++ test/acceptation/app.js
@@ -7,16 +7,16 @@ const path = require('path');
 describe('Accept Application Test', function() {
     it('Should work from A to Z', function() {
         this.timeout(4000);
-        let configPath = path.join(__dirname, '..', 'artifacts', 'application', '.pferc');
+        let configPath = path.join(__dirname, '..', 'integration', 'artifacts', 'application', '.pferc');
         let command = `node src/run.js --config "${configPath}"`;
         let options = {
         };
         let output = execSync(command, options);
         console.log(output.toString());
-        let expectedFile = path.join(__dirname, '..', 'artifacts', 'application', 'ExpectedOut.html');
+        let expectedFile = path.join(__dirname, '..', 'integration', 'artifacts', 'application', 'ExpectedOut.html');
         let expectedHtml = fs.readFileSync(expectedFile, 'UTF-8');
 
-        let resultedFile = path.join(__dirname, '..', 'artifacts', 'application', 'output.html');
+        let resultedFile = path.join(__dirname, '..', 'integration', 'artifacts', 'application', 'output.html');
         let resultingHtml = fs.readFileSync(resultedFile, 'UTF-8');
 
         assert.strictEqual(resultingHtml, expectedHtml.slice(0, -1));
