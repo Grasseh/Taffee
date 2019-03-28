2ed1f42b7ed21897a648e10a72ecbb572a8144b5
Cédric Sauriol <cedric.sauriol@hotmail.com>
2019-03-28 11:27:50 -0400

    Updated accepted extensions for markdown.

--- src/locator/MarkdownFileLocator.js
+++ src/locator/MarkdownFileLocator.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const DESIRED_EXT = '.md';
+const ACCEPTED_EXTENSIONS = ['.md', '.mdwn', '.mdown', '.mkd', '.mkdn', '.markdown', '.Rmd'];
 
 class MarkdownFileLocator {
     locateFiles(basePath) {
@@ -9,20 +9,16 @@ class MarkdownFileLocator {
             return [];
         }
 
-        if(DESIRED_EXT === path.extname(basePath)) {
-            return [basePath];
-        }
-
         return this._scanPath(basePath);
     }
 
     _scanPath(basePath) {
         let ext = path.extname(basePath);
-        if('' !== ext && DESIRED_EXT !== ext) {
+        if('' !== ext && !ACCEPTED_EXTENSIONS.includes(ext)) {
             return [];
         }
 
-        if(DESIRED_EXT === path.extname(basePath)) {
+        if(ACCEPTED_EXTENSIONS.includes(ext)) {
             return [basePath];
         }
 
